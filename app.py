86f4e382593561033b267d25a73d94b658ceb89c
kkishore5 <kaykishore5@gmail.com>
2019-09-13 16:04:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def hello_world():
     return 'Hey, we have Flask in a Docker container!'
 
+@app.route('/analysis')
+def data_analysis():
+    x = pd.DataFrame(np.random.randn(5,6), columns = list('ABCDEF'))
+    return render_template("data_analysis.html", data = x.to_html())
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
